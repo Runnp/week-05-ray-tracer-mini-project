1f8651c79f216a71a1a0831ccccfb77c4022d87a
Runnp <Runnp@users.noreply.github.com>
2026-06-14 10:26:03 +0500

    Vec

---
+++ src/vec3.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <cmath>
+#include <iostream>
+
+struct Vec3 {
+    double x, y, z;
+
+    Vec3() : x(0), y(0), z(0) {}
+    Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
+
+    // Arithmetic
+    Vec3 operator+(const Vec3& b) const { return {x+b.x, y+b.y, z+b.z}; }
+    Vec3 operator-(const Vec3& b) const { return {x-b.x, y-b.y, z-b.z}; }
+    Vec3 operator*(double t)      const { return {x*t,   y*t,   z*t};   }
+    Vec3 operator/(double t)      const { return {x/t,   y/t,   z/t};   }
+
+    // Component-wise multiply (used for colors)
+    Vec3 operator*(const Vec3& b) const { return {x*b.x, y*b.y, z*b.z}; }
+
+    Vec3 operator-() const { return {-x, -y, -z}; }
+
+    // Vector math
+    double dot(const Vec3& b)  const { return x*b.x + y*b.y + z*b.z; }
+    double lengthSq()          const { return x*x + y*y + z*z; }
+    double length()            const { return std::sqrt(lengthSq()); }
+    Vec3   normalize()         const { return *this / length(); }
+
+    // Color aliases
+    double r() const { return x; }
+    double g() const { return y; }
+    double b() const { return z; }
+};
+
+// So you can write:  2.0 * someVec
+inline Vec3 operator*(double t, const Vec3& v) { return v * t; }
