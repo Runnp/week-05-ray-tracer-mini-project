6b3bbe44a99b1c82bd7a215e8654c735bafe1871
Runnp <Runnp@users.noreply.github.com>
2026-06-14 10:26:53 +0500

    Ray + Main

---
+++ src/ray.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "vec3.h"
+
+struct Ray {
+    Vec3 origin;
+    Vec3 direction;
+
+    Ray(const Vec3& origin, const Vec3& direction)
+        : origin(origin), direction(direction) {}
+
+    // P(t) = origin + t * direction
+    Vec3 at(double t) const {
+        return origin + direction * t;
+    }
+};
