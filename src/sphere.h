deb823aac456b677eaab854be4801b008a820689
Runnp <Runnp@users.noreply.github.com>
2026-06-18 18:39:54 +0500

    Sphere

---
+++ src/sphere.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "vec3.h"
+#include "ray.h"
+
+struct HitRecord {
+    Vec3   point;    
+    Vec3   normal;   
+    double t;        
+};
+
+struct Sphere {
+    Vec3   center;
+    double radius;
+
+    Sphere(Vec3 center, double radius)
+        : center(center), radius(radius) {}
+
+    bool hit(const Ray& ray, double tMin, double tMax, HitRecord& rec) const {
+        Vec3 oc = ray.origin - center;
+        double a = ray.direction.dot(ray.direction);
+        double b = 2.0 * oc.dot(ray.direction);
+        double c = oc.dot(oc) - radius * radius;
+        double discriminant = b*b - 4*a*c;
+
+        if (discriminant < 0) return false;
+
+        double sqrtD = std::sqrt(discriminant);
+
+        double root = (-b - sqrtD) / (2.0 * a);
+        if (root < tMin || root > tMax) {
+            root = (-b + sqrtD) / (2.0 * a);
+            if (root < tMin || root > tMax) return false;
+        }
+
+        rec.t      = root;
+        rec.point  = ray.at(root);
+        rec.normal = (rec.point - center).normalize();
+        return true;
+    }
+};
