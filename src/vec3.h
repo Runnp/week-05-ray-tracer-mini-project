d69791d3bff07b2226fbec08b0e05c3825d04fc7
Runnp <Runnp@users.noreply.github.com>
2026-06-23 13:06:02 +0500

    Delete Hit

--- src/vec3.h
+++ src/vec3.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <cmath>
-#include <iostream>
+#include <cstdlib>
 
 struct Vec3 {
     double x, y, z;
@@ -8,34 +8,25 @@ struct Vec3 {
     Vec3() : x(0), y(0), z(0) {}
     Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
 
-
     Vec3 operator+(const Vec3& b) const { return {x+b.x, y+b.y, z+b.z}; }
     Vec3 operator-(const Vec3& b) const { return {x-b.x, y-b.y, z-b.z}; }
     Vec3 operator*(double t)      const { return {x*t,   y*t,   z*t};   }
     Vec3 operator/(double t)      const { return {x/t,   y/t,   z/t};   }
-    Vec3 operator+(double t)      const { return {x+t, y+t, z+t}; }
-
-
-    
     Vec3 operator*(const Vec3& b) const { return {x*b.x, y*b.y, z*b.z}; }
+    Vec3 operator+(double t)      const { return {x+t,   y+t,   z+t};   }
+    Vec3 operator-()              const { return {-x,    -y,    -z};     }
 
-    Vec3 operator-() const { return {-x, -y, -z}; }
+    double dot(const Vec3& b) const { return x*b.x + y*b.y + z*b.z; }
+    double lengthSq()         const { return x*x + y*y + z*z; }
+    double length()           const { return std::sqrt(lengthSq()); }
+    Vec3   normalize()        const { return *this / length(); }
 
-    
-    double dot(const Vec3& b)  const { return x*b.x + y*b.y + z*b.z; }
-    double lengthSq()          const { return x*x + y*y + z*z; }
-    double length()            const { return std::sqrt(lengthSq()); }
-    Vec3   normalize()         const { return *this / length(); }
-
-    
     double r() const { return x; }
     double g() const { return y; }
     double b() const { return z; }
 };
 
 inline Vec3 operator*(double t, const Vec3& v) { return v * t; }
-
-#include <cstdlib>
 
 inline double randomDouble() {
     return std::rand() / (RAND_MAX + 1.0);
